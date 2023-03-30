a5e4e78433697149a39661f07d2940eef8f655f7
Davide <email@gmail.com>
2023-03-30 12:41:51 +0200

    Es.1 complete py+st

--- danny_G/patente/app.py
+++ danny_G/patente/app.py
@@ -1,65 +1,73 @@
 import streamlit as st
+import datetime
+from datetime import timedelta
+import time
 
 st.title('Noleggia auto')
 
-if 'False' not in st.session_state:
-    st.session_state.disabled = True
+disabled_patente = True
+disabled_age_input = True
+disabled_slider = True
+disabled_data = True
+
+option = st.radio (
+    'Come vuoi inserire la tua età?',
+    ('Input', 'Slider','Data'), horizontal=True)
+
+if option == 'Input':
+   disabled_age_input = False 
+elif option == 'Slider':
+   disabled_slider = False 
+else :
+   disabled_data = False 
+   
+
+age_input=st.number_input('Inserisci la tua età', 0, 120, disabled = disabled_age_input)
+age_slider=st.slider('Seleziona la tua età', 0, 100, 0, disabled= disabled_slider)  
+age_data= st.date_input( "Inserisci la tua data di nascita",
+                            min_value = datetime.date(1900, 1, 1),
+                            max_value = datetime.datetime.today().date(), 
+                            disabled= disabled_data)
+
+
+
+if age_input>0 or age_slider>0 or age_data != datetime.datetime.today().date() :
+    patente=st.radio(
+                'Hai la patente?',
+                ['SI','NO'], 
+                
+            )
+
+
+maggiorenne = datetime.datetime.today().date()-timedelta(days=6570)
     
-age_select=st.number_input('Inserisci la tua età', 0, 120,)
-st.checkbox("Disable radio widget",key="disabled")
-#age=st.slider('Seleziona la tua età', 0, 100, 18)
-patente=st.radio(
-            'Hai la patente?',
-            ['SI','NO'], 
-            key="False",
-            disabled=st.session_state.disabled,
-        )
+if age_data < maggiorenne:
+    data_go = '1'
+else :
+    data_go = '0'
 
 
-# if "visibility" not in st.session_state:
-#     st.session_state.visibility = "visible"
-#     st.session_state.disabled = False
-#     st.session_state.horizontal = False
+def go_nogo (age_input,age_slider,data_go,patente):
 
-# col1, col2 = st.columns(2)
-
-# with col1:
-#     st.checkbox("Disable radio widget", key="disabled")
-#     st.checkbox("Orient radio options horizontally", key="horizontal")
-
-# with col2:
-#     st.radio(
-#         "Set label visibility 👇",
-#         ["visible", "hidden", "collapsed"],
-#         key="visibility",
-#         label_visibility=st.session_state.visibility,
-#         disabled=st.session_state.disabled,
-#         horizontal=st.session_state.horizontal,
-#     )
+    if (((age_input >=18) or (age_slider>=18) or (data_go=='1') ) and (patente=='SI')):
+        go_nogo='daje!'
+    elif (((age_input >=18) or (age_slider>=18) or (data_go=='1')) and  (patente=='NO')):
+        go_nogo='devi prima prendere la patente'
+    else : 
+        go_nogo='sei un bimbo, cresci e ripassa'
+    return(go_nogo)
 
 
-# def go_nogo (age_select,patente):
-#     if (age_select < 0):
-#         go_nogo='come fai ad avere meno di zero anni?'
-
-#     else: 
-#         if (age_select >=18 and patente=='SI'):
-#             go_nogo='daje!'
-#         elif (age_select >=18 and patente=='NO'):
-#             go_nogo='devi prima prendere la patente'
-#         else : 
-#             go_nogo='sei un bimbo, cresci e ripassa'
-#     return(go_nogo)
-
-
-# if st.button('Verifica'):
+if st.button('Verifica'):
     
-#     while (go_nogo(age_select,patente) != 'daje'):
-#         st.snow()
-#     else:
-#         st.balloons()
+    with st.spinner('Stiamo controllando'):
+        time.sleep(3)
 
-#     st.write(go_nogo(age_select,patente))
-
+    if (go_nogo(age_input,age_slider,data_go,patente) == 'daje!'):
+        st.balloons()
+        st.success(go_nogo(age_input,age_slider,data_go,patente))
+    else:
+        st.error(go_nogo(age_input,age_slider,data_go,patente))
+    
 
 
